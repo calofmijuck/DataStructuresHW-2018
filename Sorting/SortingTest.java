9a779c7c9ec6d5ada81bc4827cc72e876485bac3
Sungchan Yi <38686321+calofmijuck@users.noreply.github.com>
2018-05-12 12:40:31 +0900

    Update SortingTest.java

--- Sorting/SortingTest.java
+++ Sorting/SortingTest.java
@@ -2,7 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class SortingTest {
-	static int[] arr, tmp1, tmp2; // temporary arrays to save copying time for recursive calls
+	static Random randInt = new Random();
+	static int size;
+
 	public static void main(String args[]) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try	{
@@ -22,8 +24,6 @@ public class SortingTest {
 				Random rand = new Random();	// 난수 인스턴스를 생성한다.
 
 				value = new int[numsize];	// 배열을 생성한다.
-				tmp1 = new int[numsize];
-				tmp2 = new int[numsize];
 				for (int i = 0; i < value.length; i++)	// 각각의 배열에 난수를 생성하여 대입
 					value[i] = rand.nextInt(rmaximum - rminimum + 1) + rminimum;
 			} else {
@@ -31,8 +31,6 @@ public class SortingTest {
 				int numsize = Integer.parseInt(nums);
 
 				value = new int[numsize];	// 배열을 생성한다.
-				tmp1 = new int[numsize];
-				tmp2 = new int[numsize];
 				for (int i = 0; i < value.length; i++)	// 한줄씩 입력받아 배열원소로 대입
 					value[i] = Integer.parseInt(br.readLine());
 			}
@@ -40,9 +38,7 @@ public class SortingTest {
 			// 숫자 입력을 다 받았으므로 정렬 방법을 받아 그에 맞는 정렬을 수행한다.
 			while (true) {
 				int[] newvalue = (int[])value.clone();	// 원래 값의 보호를 위해 복사본을 생성한다.
-				arr = (int[]) value.clone(); // copy
 				String command = br.readLine();
-
 				long t = System.currentTimeMillis();
 				switch (command.charAt(0)) {
 					case 'B':	// Bubble Sort
@@ -73,46 +69,47 @@ public class SortingTest {
 					System.out.println((System.currentTimeMillis() - t) + " ms");
 				} else {
 					// 난수가 아닐 경우 정렬된 결과값을 출력한다.
-					for(int i = 0; i < newvalue.length; i++) {
+					for (int i = 0; i < newvalue.length; i++) {
 						System.out.println(newvalue[i]);
 					}
 				}
 			}
-		} catch (Exception e) {
+		} catch (IOException e) {
 			System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
-			e.printStackTrace();
 		}
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	private static int[] DoBubbleSort(int[] value) {
-		int tmp;
-		for(int i = 0; i < value.length - 1; i++) {
-			for(int j = 0; j < value.length - i - 1; j++) {
-				if(value[j] > value[j + 1]) { // compare and swap
-					tmp = value[j + 1];
+		int cmp = 0; // counts number of comparisions
+		int temp; // temporary holder for swapping
+		for(int i = 0; i < value.length; ++i) {
+			cmp = 0;
+			for(int j = 0; j < value.length - i - 1; ++j) {
+				if(value[j] > value[j + 1]) {
+					temp = value[j + 1];
 					value[j + 1] = value[j];
-					value[j] = tmp;
+					value[j] = temp;
+					cmp++;
 				}
+				// if(cmp == 0) {
+				// 	return value;
+				// }
 			}
 		}
-		// value는 정렬안된 숫자들의 배열이며 value.length 는 배열의 크기가 된다.
-		// 결과로 정렬된 배열은 리턴해 주어야 하며, 두가지 방법이 있으므로 잘 생각해서 사용할것.
-		// 주어진 value 배열에서 안의 값만을 바꾸고 value를 다시 리턴하거나
-		// 같은 크기의 새로운 배열을 만들어 그 배열을 리턴할 수도 있다.
 		return value;
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	private static int[] DoInsertionSort(int[] value) {
-		int tmp;
-		for(int i = 0; i < value.length; i++) {
-			for(int j = i; j > 0; j--) {
-				if(value[j] < value[j - 1]) { // Is this the right place to insert?
-					tmp = value[j - 1];
+		int temp; // swapping
+		for(int i = 1; i < value.length; ++i) {
+			for(int j = i; j > 0; --j) {
+				if(value[j] < value[j - 1]) {
+					temp = value[j - 1];
 					value[j - 1] = value[j];
-					value[j] = tmp;
-				} else { // stop insertion
+					value[j] = temp;
+				} else {
 					break;
 				}
 			}
@@ -121,85 +118,114 @@ public class SortingTest {
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	private static int[] DoHeapSort(int[] value)
-	{
-		// TODO : Heap Sort 를 구현하라.
-		return (value);
+	private static int[] DoHeapSort(int[] value) {
+		size = value.length;
+		// build max heap
+		for(int i = size / 2; i >= 0; --i) {
+			value = maxHeapify(value, i);
+		}
+		int temp, idx = value.length - 1;
+		for(int i = 0; i < idx; ++i) {
+			temp = value[idx - i];
+			value[idx - i] = value[0];
+			value[0] = temp;
+			--size;
+			value = maxHeapify(value, 0);
+		}
+		return value;
+	}
+
+	private static int[] maxHeapify(int[] value, int idx) {
+		int left = 2 * idx + 1, right = 2 * idx + 2;
+		int temp;
+		if(left < size)  { // left child exists
+            if(right < size && value[left] < value[right]) { // right child exists and right child is greater
+                left = right; // larger child is stored
+            }
+            if(value[idx] < value[left]) { // compare with current child
+                temp = value[idx];
+                value[idx] = value[left];
+                value[left] = temp;
+                value = maxHeapify(value, left);
+            }
+        }
+		return value;
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	private static int[] DoMergeSort(int[] value) {
-		MergeSort(1, value.length);
-		return arr;
+		return MergeSort(value, 0, value.length - 1);
 	}
 
-	private static void MergeSort(int start, int end) {
-		if(start < end) {
-			int mid = (int) Math.ceil((start + end) / 2);
-			MergeSort(start, mid);
-			MergeSort(mid + 1, end);
-			Merge(start, mid, end);
-		}
-	}
-
-	private static void Merge(int start, int mid, int end) {
-		int len1 = mid - start + 1, len2 = end - mid;
-		for(int i = 0; i < len1; i++) { // copy array
-			tmp1[i] = arr[start + i - 1];
-		}
-		for(int j = 0; j < len2; j++) {
-			tmp2[j] = arr[mid + j];
-		}
-		int i = 0, j = 0, k = start - 1; // pointer indices for tmp1, tmp2, result array
-		while(i < len1 && j < len2) {
-			if(tmp1[i] <= tmp2[j]) { // stable sort
-				arr[k] = tmp1[i];
-				i++;
-			} else {
-				arr[k] = tmp2[j];
-				j++;
+	private static int[] MergeSort(int[] value, int start, int end) {
+		if(start >= end) {
+			return value;
+		} else {
+			int mid = (start + end) / 2;
+			int[] left = MergeSort(value, start, mid);
+			int[] right = MergeSort(value, mid + 1, end);
+			// Merge
+			int[] tmp = new int[end - start + 1];
+			int ptrLeft = start, ptrRight = mid + 1, ptrNew = 0;
+			while(ptrLeft <= mid && ptrRight <= end) {
+				if(left[ptrLeft] <= right[ptrRight]) {
+					tmp[ptrNew] = left[ptrLeft];
+					ptrLeft++;
+				} else {
+					tmp[ptrNew] = right[ptrRight];
+					ptrRight++;
+				}
+				ptrNew++;
 			}
-			k++;
+			while(ptrLeft <= mid) {
+				tmp[ptrNew] = left[ptrLeft];
+				ptrNew++;
+				ptrLeft++;
+			}
+			while(ptrRight <= end) {
+				tmp[ptrNew] = right[ptrRight];
+				ptrNew++;
+				ptrRight++;
+			}
+			for(int i = start; i <= end; ++i) {
+				value[i] = tmp[i - start];
+			}
+			return value;
 		}
-		// copy the remaining elements
-		for(; i < len1; i++, k++) {
-			arr[k] = tmp1[i];
-		}
-		for(; j < len2; j++, k++) {
-			arr[k] = tmp2[j];
-		}
+
 	}
+
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	private static int[] DoQuickSort(int[] value) {
-		QuickSort(0, value.length - 1);
-		return arr;
+		value = QuickSort(value, 0, value.length - 1);
+		return value;
 	}
 
-	private static int partition(int low, int high) { // partitions from index low to high
-		int pivot = arr[high]; // selecting the last element as pivot
-		int i = low - 1; // i counts: (the number of elements smaller than pivot) - 1 + low
-		// thus i + 1 will be the correct place of the pivot element in the sorted array
-		int tmp;
-		for(int j = low; j < high; j++) {
-			if(arr[j] < pivot) { // put all elements smaller than pivot on the left part of array
-				i++;
-				tmp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = tmp;
+	private static int[] QuickSort(int[] value, int start, int end) {
+		if(start < end) {
+			// partition the Array
+			int pivot = value[end]; // last element as pivot
+			int i = start - 1; // i counts: (the number of elements smaller than pivot) - 1 + low
+			// thus i + 1 will be the correct place of the pivot element in the sorted array
+			int temp;
+			for(int j = start; j < end; ++j) {
+				if(value[j] < pivot) { // elements smaller than pivot are moved to the left parition
+					++i;
+					temp = value[i];
+					value[i] = value[j];
+					value[j] = temp;
+				}
 			}
-		}
-		tmp = arr[i + 1];
-		arr[i + 1] = pivot; // place the pivot at i + 1
-		arr[high] = tmp;
-		return i + 1; // correct location of pivot in the array
-	}
-
-	private static void QuickSort(int low, int high) {
-		if(low < high) {
-			int partIndex = partition(low, high); // partitioning index
-			QuickSort(low, partIndex - 1); // Sort left
-			QuickSort(partIndex + 1, high); // Sort right
+			temp = value[i + 1];
+			value[i + 1] = pivot; // place the pivot at i + 1
+			value[end] = temp;
+			int partIndex = i + 1; // parition index
+			int[] res = QuickSort(value, start, partIndex - 1); // sort left
+			res = QuickSort(res, partIndex + 1, end); // sort right
+			return res;
+		} else {
+			return value;
 		}
 	}
 
@@ -212,25 +238,26 @@ public class SortingTest {
 		return value;
 	}
 
-	private static int[] countSort(int[] arr, int digit) {
-		int[] res = new int[arr.length];
-		int[] count = new int[20]; // number of occurrences of each digit from -9 to 9
-		for(int i = 0; i < arr.length; i++) {
-			count[(arr[i] / digit) % 10 + 9]++; // occurrence of digit
+	private static int[] countSort(int[] value, int digit) {
+		int[] res = new int[value.length];
+		int[] count = new int[20]; // number of occurences of each digit from -9 to 9
+		for(int i = 0; i < value.length; ++i) {
+			count[(value[i] / digit) % 10 + 9]++; // occurrnece of each digit
 		}
-		for(int i = 1; i < 20; i++) {
-			count[i] += count[i - 1]; // accumulate occurrences to get the correct index
+		for(int i = 1; i < 20; ++i) {
+			count[i] += count[i - 1]; // accumulate occurences to get the correct index
 		}
-		for(int i = arr.length - 1; i >= 0; i--) { // iterate backwards for stable sorting
-			int loc = (arr[i] / digit) % 10 + 9; // digit of current element
-			res[count[loc] - 1] = arr[i]; // count[loc] - 1 is the correct place
-			count[loc]--; // decrement
+		for(int i = value.length - 1; i >= 0; --i) { // iterate backwards for stable sorting
+			int loc = (value[i] / digit) % 10 + 9; //digit of current element
+			res[count[loc] - 1] = value[i];
+			count[loc]--;
 		}
 		return res;
 	}
 
 	private static int getMax(int[] arr) {
-		// returns the element with largest absolute value, to figure out the largest existing digit from the input
+		// returns the element with largest absolute value
+		// to figure out the largest exponent
 		int max = Math.abs(arr[0]);
 		for(int i = 1; i < arr.length; i++) {
 			if(Math.abs(arr[i]) > max) {
